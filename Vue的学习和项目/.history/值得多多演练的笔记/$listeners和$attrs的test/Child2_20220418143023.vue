01cffe8b5de0509bd936dc372534413aba608971
try <254691782@qq.com>
2022-04-19 05:17:47 +0800

    测试用的东东

---
+++ Vue的学习和项目/.history/值得多多演练的笔记/$listeners和$attrs的test/Child2_20220418143023.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="child-2">
+        <span>这是孙子组件C</span>
+        <p>in child2:</p>
+        <p>props: {{pChild2}}</p>
+        <p>$attrs: {{$attrs}}</p>
+        <hr>
+    </div>
+</template>
+<script>
+export default {
+    props: ['pChild2'],
+    data () {
+        return {};
+    },
+    inheritAttrs: false,
+    mounted () {
+        this.$emit('test2');
+    }
+};
+</script>
